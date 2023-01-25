d480cc2e6d7da45b5c132f2ec47b4c9ca7cc5b34
Jeremy <s0107592@tukwila.wednet.edu>
2023-01-25 13:16:50 -0800

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,174 @@
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class Robot extends TimedRobot {
+  private final WPI_TalonFX left = new WPI_TalonFX(2); // left drive motor
+  private final WPI_TalonFX external = new WPI_TalonFX(1); // external motor
+  private final WPI_TalonFX right = new WPI_TalonFX(0); // right drive motor
+  private final DifferentialDrive drive = new DifferentialDrive(left, right);
+  private final XboxController controller = new XboxController(0);
+  private final Timer timer = new Timer(); 
+  private final ADIS16448_IMU gyro = new ADIS16448_IMU(); // RoboRIO-mounted gyroscope
+ 
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    initialize();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // updates variables
+    double time = timer.get();
+    double positionExternal = external.getSelectedSensorPosition(0);
+    double positionLeft = left.getSelectedSensorPosition(0);
+    double positionRight = right.getSelectedSensorPosition(0);
+    double angle = gyro.getGyroAngleZ();
+
+    // publishes updated variables to the dashboard
+    SmartDashboard.putNumber("Clock",  time);
+    SmartDashboard.putNumber("Encoder (External)", positionExternal);
+    SmartDashboard.putNumber("Encoder (Left)", positionLeft);
+    SmartDashboard.putNumber("Encoder (Right)", positionRight);
+    SmartDashboard.putNumber("Angle", angle);
+
+    /* Bugs
+     * arcadeDrive parameters are flipped
+     * Movement is also inverse, hence, the negatives
+     */
+
+    // runs the external motor at 50% until one motor rotation
+    if (positionExternal >= 55578) {
+      drive.arcadeDrive(0, 0);
+    } 
+    if (positionExternal < 55578) {
+      drive.arcadeDrive(0, -1);
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+    initialize();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // updates variables
+    double leftStickY = controller.getLeftY();
+    double leftStickX = controller.getLeftX();
+    double positionExternal = external.getSelectedSensorPosition(0);
+    double positionLeft = left.getSelectedSensorPosition(0);
+    double positionRight = right.getSelectedSensorPosition(0);
+    double angle = gyro.getGyroAngleZ();
+    
+    // publishes updated variables to the dashboard
+    SmartDashboard.putNumber("leftStickY", leftStickY);
+    SmartDashboard.putNumber("leftStickX", leftStickX);
+    SmartDashboard.putNumber("Encoder (External)", positionExternal);
+    SmartDashboard.putNumber("Encoder (Left)", positionLeft);
+    SmartDashboard.putNumber("Encoder (Right)", positionRight);
+    SmartDashboard.putNumber("Angle", angle);
+    
+    // sets the drive motors based on the left joystick inputs
+    drive.arcadeDrive(leftStickX, leftStickY, true);
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+
+  // runs manufacturer recommended startup commands for Falcon 500 motors. Should be run at startup for all motors.
+  public void motorStartUp(WPI_TalonFX motor) {
+    motor.configFactoryDefault();
+    motor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0 , 30);
+    motor.configNeutralDeadband(0.01, 30);
+    motor.setSensorPhase(false);
+    motor.setInverted(false);
+    motor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
+    motor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
+    motor.configNominalOutputForward(0, 30);
+    motor.configNominalOutputReverse(0, 30);
+    motor.configPeakOutputForward(1, 30);
+    motor.configPeakOutputReverse(-1, 30);
+    motor.selectProfileSlot(0, 0);
+    motor.setSelectedSensorPosition(0, 0, 30);
+  }
+
+  public void initialize() {
+    motorStartUp(external);
+
+    // PID coefficients 
+    external.config_kF(0, 0, 30); 
+    external.config_kP(0, 1, 30);
+    external.config_kI(0, 0.005, 30);
+    external.config_kD(0, 10, 30);
+
+    // motion magic parameters
+    external.configMotionCruiseVelocity(20000, 30);
+    external.configMotionAcceleration(6000, 30);
+
+    // brake versus coast
+    external.setNeutralMode(NeutralMode.Brake); 
+
+    motorStartUp(left);
+    left.config_kF(0, 0, 30);
+    left.config_kP(0, 1, 30);
+    left.config_kI(0, 0.005, 30);
+    left.config_kD(0, 10, 30);
+    left.configMotionCruiseVelocity(20000, 30);
+    left.configMotionAcceleration(6000, 30);
+    left.setNeutralMode(NeutralMode.Brake);
+
+    motorStartUp(right);
+    right.config_kF(0, 0, 30);
+    right.config_kP(0, 1, 30);
+    right.config_kI(0, 0.005, 30);
+    right.config_kD(0, 10, 30);
+    right.configMotionCruiseVelocity(20000, 30);
+    right.configMotionAcceleration(6000, 30);
+    right.setNeutralMode(NeutralMode.Brake);
+
+    gyro.calibrate(); // sets the gyro angle to 0 based on the current robot position
+    timer.start();
+    timer.reset(); // sets the timer to 0
+    CameraServer.startAutomaticCapture(); // starts the webcam stream
+
+    // initializes values on dashboard
+    SmartDashboard.putNumber("Angle", gyro.getGyroAngleZ());
+    SmartDashboard.putNumber("Time", timer.get());
+    SmartDashboard.putNumber("Encoder (External)", external.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("Encoder (Left)", left.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("Encoder (Right)", right.getSelectedSensorPosition(0));
+  }
+}
